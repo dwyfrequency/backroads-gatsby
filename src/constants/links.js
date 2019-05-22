025102dc637d0965561019ebd2de73138ae59178
dwyfrequency <jdwy215@gmail.com>
2019-05-22 15:41:43 -0400

    adding css files for footer and navbar

---
+++ src/constants/links.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: "/",
+    text: "home",
+  },
+  {
+    path: "/rooms",
+    text: "rooms",
+  },
+  {
+    path: "/blog",
+    text: "blog",
+  },
+  {
+    path: "/contact",
+    text: "contact",
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { FaAdobe } from 'react-icons/fa'
 
 export default () => (
   <Layout>
     <div>hello from backroads</div>
+    <FaAdobe />
   </Layout>
 )
