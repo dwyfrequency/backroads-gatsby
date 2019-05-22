2c647773128529ddea41333238a1ca0aadb0c52c
dwyfrequency <jdwy215@gmail.com>
2019-05-22 16:15:07 -0400

    rending links in navbar onclick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../css/navbar.module.css'
+import { FaAlignRight, FaBluetooth } from 'react-icons/fa'
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+import logo from '../images/logo.svg'
+import { Link } from 'gatsby'
 
 const Navbar = () => {
+  const [isOpen, setNav] = useState(false)
+
+  const toggleNav = () => {
+    console.log('clicked')
+    setNav(prevIsOpen => !prevIsOpen)
+  }
+
   return (
-    <div>
-      <h1>i'm navbar</h1>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo" />
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            {/* is the icon on the right of the screen */}
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+        <ul
+          className={
+            isOpen ? `${styles.navLinks} ${styles.showNav}` : styles.navLinks
+          }
+        >
+          {links.map(({ path, text }) => (
+            <li key={text}>
+              <Link to={path}>{text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
