31052055a399e138a28bc67aadac250a16d3388b
dwyfrequency <jdwy215@gmail.com>
2019-05-22 17:38:50 -0400

    adding completed hero, navbar and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { FaAdobe } from 'react-icons/fa'
+import SimpleHero from '../components/SimpleHero'
+import Banner from '../components/Banner'
+import { Link } from 'gatsby'
 
 export default () => (
   <Layout>
-    <div>hello from backroads</div>
-    <FaAdobe />
+    <SimpleHero>
+      <Banner title="continue exploring" info="lorem ipsum">
+        <Link to="/tours" className="btn-white">
+          Explore Tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
