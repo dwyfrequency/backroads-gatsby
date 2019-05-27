1dbabecf427fc5909a53e8d5c212debba316f254
dwyfrequency <jdwy215@gmail.com>
2019-05-27 12:37:07 -0400

    adding images component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Images from '../components/Images'
+import Images from '../examples/Images'
 
 const blog = () => {
   return (
