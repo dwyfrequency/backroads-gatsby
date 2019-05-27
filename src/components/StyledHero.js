6764db6e3a47eba040543d5e00e76644f45c896c
dwyfrequency <jdwy215@gmail.com>
2019-05-27 13:32:32 -0400

    created styled hero

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -10,11 +10,11 @@ const StyledHero = ({ img, className, children, home }) => {
 }
 
 export default styled(StyledHero)`
-min-height: ${props => (props.home ? 'calc(100vh - 62px)' : '')};
-background: ${props =>
-  props.home
-    ? 'linear - gradient(rgba(63, 208, 212, 0.7), rgba(0, 0, 0, 0.7))'
-    : ''}
+  min-height: ${props => (props.home ? 'calc(100vh - 62px)' : '')};
+  background: ${props =>
+    props.home
+      ? 'linear - gradient(rgba(63, 208, 212, 0.7), rgba(0, 0, 0, 0.7))'
+      : ''};
   background-position: center;
   background-size: cover;
   opacity: 1 !important;
