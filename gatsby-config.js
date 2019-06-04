da047e84bd1dd88e84c700638bc9a3f113f6977b
dwyfrequency <jdwy215@gmail.com>
2019-06-04 12:16:27 -0400

    adding tokens for cotentful and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -22,6 +26,14 @@ module.exports = {
       options: {
         name: `pages`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
