0747f6b7bc8ec35969115192ffcc9d92d8050f0d
dwyfrequency <jdwy215@gmail.com>
2019-06-05 11:51:02 -0400

    adding default image for tour

--- src/components/tours/Tour.js
+++ src/components/tours/Tour.js
@@ -3,13 +3,35 @@ import Image from 'gatsby-image'
 import styles from '../../css/tour.module.css'
 import { FaMap } from 'react-icons/fa'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-
 import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const getImage = graphql`
+  query {
+    file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Tour = ({ tour }) => {
+  const data = useStaticQuery(getImage)
+  const img = data.file.childImageSharp.fluid
   const { name, price, country, days, slug, images } = tour
+  console.log(data)
 
-  let mainImage = images[0].fluid
+  // let mainImage
+  // if (images) {
+  //   mainImage = images[0].fluid
+  // } else {
+  //   mainImage = img
+  // }
+  // creating a default image in case
+  let mainImage = images ? images[0].fluid : img
 
   return (
     <article className={styles.tour}>
