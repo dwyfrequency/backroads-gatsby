3370aee079609228d992992c2634451fef771445
dwyfrequency <jdwy215@gmail.com>
2019-06-05 11:53:47 -0400

    added default image in styledhero component

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import BackgroundImage from 'gatsby-background-image'
+
+import { useStaticQuery, graphql } from 'gatsby'
+
+const getImage = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
+
 const StyledHero = ({ img, className, children, home }) => {
+  const data = useStaticQuery(getImage)
+
   return (
-    <BackgroundImage className={className} fluid={img} home={home}>
+    <BackgroundImage
+      className={className}
+      fluid={img || data.defaultBcg.childImageSharp.fluid}
+      home={home}
+    >
       {children}
     </BackgroundImage>
   )
@@ -14,7 +35,7 @@ export default styled(StyledHero)`
   background: ${props =>
     props.home
       ? 'linear-gradient(rgba(63, 208, 212, 0.7), rgba(0, 0, 0, 0.7))'
-      : ''};
+      : 'none'};
   background-position: center;
   background-size: cover;
   opacity: 1 !important;
